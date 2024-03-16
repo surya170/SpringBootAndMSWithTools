d942c8538216144e73e538efe6714036d0c57c64
SURYA <61978643+surya170@users.noreply.github.com>
2024-03-16 16:18:40 +0530

    method added by developer-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void method-1(){
+      System.out.println("Hello World");
+    }
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         if(Constant.data < 15) {
